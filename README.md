f464c9452c661d0c0302f141e461c773b1b35ddf
Jonathan Ong <jonathanrichardong@gmail.com>
2014-07-30 17:58:51 -0700

    init

---
+++ README.md
@@ -0,0 +1,88 @@
+
+# glitter
+
+[![NPM version][npm-image]][npm-url]
+[![build status][travis-image]][travis-url]
+[![Test coverage][coveralls-image]][coveralls-url]
+[![Gittip][gittip-image]][gittip-url]
+
+A utility to interact with remote repositories seamlessly.
+Supports fetching remotes, getting references/tags/branches
+from either the remote or the local copy, and copying files.
+The purpose of this is as a basis for [normalize-proxy](https://github.com/normalize/proxy.js)
+and perhaps could be used for any package management utility.
+
+## API
+
+### Glitter.folder
+
+Where the repositories are cloned to.
+
+### var glitter = new Glitter(remote, user, repo)
+
+Create a new instance. Currently supported remotes are:
+
+- `github` - set your optional GitHub credentials as the `GLITTER_GITHUB` environmental variable
+
+### glitter.install().then( => )
+
+`git clone`
+
+Clone the remote repository locally.
+Does not check whether the repository has already been cloned.
+
+### glitter.isInstall().then( !!installed => )
+
+Checks whether a repository is installed.
+
+### glitter.update().then( !!updated => )
+
+`git fetch -f --all`
+
+Update all the branches and references of the local copy.
+`updated` is whether any references were updated.
+
+### glitter.show(reference, remote).then( hash'' => )
+
+`git show`
+
+Lookup a commit sha from a reference.
+If `remote` is true, will `.update()` from the origin if no references are found.
+This requires a local copy of the repository to be installed.
+
+### glitter.copy(reference, folder).then( => )
+
+`git archive <reference> | tar -x -C <folder>`
+
+Copy the repository at `reference` to `folder`.
+Requires a local copy.
+
+### glitter.getReferences(remote).then( [refs] => )
+
+`git ls-remote` and `git show-ref`
+
+Gets all `HEAD` and tag references,
+References is an array of references of the form `[<name>, <commit sha>]`.
+If `remote` is true, gets the references directly from the remote.
+
+### glitter.getHeads(remote).then( [refs] => )
+
+`.getReferences(remote)`, but only HEAD references are returned.
+
+### glitter.getTags(remote).then( [refs] => )
+
+`.getReferences(remote)`, but only tags are returned.
+
+### glitter.getVersions(strict, remote).then( [versions] => )
+
+`.getTags(remote)`, except only valid semantic versions are returned.
+Optionally, you may return only `strict` semantic versions.
+
+[npm-image]: https://img.shields.io/npm/v/glitter.svg?style=flat
+[npm-url]: https://npmjs.org/package/glitter
+[travis-image]: https://img.shields.io/travis/repo-utils/glitter.svg?style=flat
+[travis-url]: https://travis-ci.org/repo-utils/glitter
+[coveralls-image]: https://img.shields.io/coveralls/repo-utils/glitter.svg?style=flat
+[coveralls-url]: https://coveralls.io/r/repo-utils/glitter?branch=master
+[gittip-image]: https://img.shields.io/gittip/jonathanong.svg?style=flat
+[gittip-url]: https://www.gittip.com/jonathanong/
---
+++ test/index.js
@@ -0,0 +1,202 @@
+
+var fs = require('fs')
+var path = require('path')
+var assert = require('assert')
+
+var Glitter = require('..')
+var out = path.join(__dirname, '..', 'cache2')
+
+require('rimraf').sync(Glitter.folder)
+require('rimraf').sync(out)
+
+describe('Glitter', function () {
+  describe('GitHub', function () {
+    var glitter
+
+    describe('glitter(remove, user, repo)', function () {
+      it('.folder, .url', function () {
+        glitter = Glitter('gh', 'jonathanong', 'routington')
+        assert(glitter.folder)
+        assert(glitter.url)
+      })
+
+      describe('.install()', function () {
+        it('should support double .then()s', function () {
+          glitter.install().then(function () {
+            assert(fs.existsSync(glitter.folder))
+          })
+
+          return glitter.install().then(function () {
+            return new Promise(function (resolve) {
+              setImmediate(function () {
+                assert(!glitter._installPromise)
+                resolve()
+              })
+            })
+          })
+        })
+      })
+
+      describe('.isInstalled()', function () {
+        it('should return true if installed', function () {
+          return glitter.isInstalled().then(function (yes) {
+            assert(yes)
+          })
+        })
+
+        it('should return false if not installed', function () {
+          return Glitter('gh', 'klajsdlkfjasdf', 'alkjalksdjf').isInstalled(function (yes) {
+            assert(!yes)
+          })
+        })
+      })
+
+      describe('.update()', function () {
+        it('should return false when there are no updates', function () {
+          return glitter.update().then(function (updates) {
+            assert(!updates)
+          })
+        })
+      })
+
+      describe('.show', function () {
+        it('(sha, remote=false)', function () {
+          return glitter.show('08c7518caf83').then(function (sha) {
+            assert.equal('08c7518caf831f10c667e91e978c5103349bfd7f', sha)
+          })
+        })
+
+        it('(branch, remote=false)', function () {
+          return glitter.show('master').then(function (sha) {
+            assert(/^[a-z0-9]{40}$/.test(sha))
+          })
+        })
+
+        it('(sha, remote=true)', function () {
+          return glitter.show('08c7518caf83', true).then(function (sha) {
+            assert.equal('08c7518caf831f10c667e91e978c5103349bfd7f', sha)
+          })
+        })
+
+        it('(branch, remote=true)', function () {
+          return glitter.show('master', true).then(function (sha) {
+            assert(/^[a-z0-9]{40}$/.test(sha))
+          })
+        })
+
+        it('(sha, remote=false) missing', function () {
+          // throws an error
+          return glitter.show('abcabcabcabc', false).then(function () {
+            assert(false)
+          }, function () {})
+        })
+
+        it('(sha, remote=true) missing', function () {
+          // throws an error
+          return glitter.show('abcabcabcabc', true).then(function () {
+            assert(false)
+          }, function () {})
+        })
+      })
+
+      describe('.getReferences', function () {
+        it('(remote=false)', function () {
+          return glitter.getReferences(false).then(function (references) {
+            assert(references.heads.length)
+            assert(references.heads.some(function (reference) {
+              return reference[0] === 'master'
+            }))
+          })
+        })
+
+        it('(remote=true)', function () {
+          return glitter.getReferences(true).then(function (references) {
+            assert(references.heads.length)
+            assert(references.heads.some(function (reference) {
+              return reference[0] === 'master'
+            }))
+          })
+        })
+
+        it('(reference, remote=false)', function () {
+          return glitter.getReferences('1.0.0', false).then(function (references) {
+            assert.equal(1, references.tags.length)
+          })
+        })
+
+        it('(reference, remote=true)', function () {
+          return glitter.getReferences('1.0.0', true).then(function (references) {
+            assert.equal(1, references.tags.length)
+          })
+        })
+      })
+
+      describe('.getHeads', function () {
+        it('(remote=false)', function () {
+          return glitter.getHeads(false).then(function (heads) {
+            return assert(heads.some(function (head) {
+              return head[0] === 'master'
+                && /^[a-z0-9]{40}$/.test(head[1])
+            }))
+          })
+        })
+
+        it('(remote=true)', function () {
+          return glitter.getHeads(true).then(function (heads) {
+            return assert(heads.some(function (head) {
+              return head[0] === 'master'
+                && /^[a-z0-9]{40}$/.test(head[1])
+            }))
+          })
+        })
+      })
+
+      describe('.getTags', function () {
+        it('(remote=false)', function () {
+          return glitter.getTags(false).then(function (tags) {
+            return assert(tags.some(function (tag) {
+              return tag[0] === '1.0.0'
+                && /^[a-z0-9]{40}$/.test(tag[1])
+            }))
+          })
+        })
+
+        it('(remote=true)', function () {
+          return glitter.getTags(true).then(function (tags) {
+            return assert(tags.some(function (tag) {
+              return tag[0] === '1.0.0'
+                && /^[a-z0-9]{40}$/.test(tag[1])
+            }))
+          })
+        })
+      })
+
+      describe('.getVersions', function () {
+        it('(remote=false)', function () {
+          return glitter.getVersions(false).then(function (versions) {
+            assert(~versions.indexOf('1.0.0'))
+            assert(~versions.indexOf('0.1.3'))
+            assert(versions.indexOf('1.0.0') < versions.indexOf('0.1.3'))
+          })
+        })
+
+        it('(remote=true)', function () {
+          return glitter.getVersions(true).then(function (versions) {
+            assert(~versions.indexOf('1.0.0'))
+            assert(~versions.indexOf('0.1.3'))
+            assert(versions.indexOf('1.0.0') < versions.indexOf('0.1.3'))
+          })
+        })
+      })
+
+      describe('.copy(reference, folder)', function () {
+        it('should copy', function () {
+          var folder = path.join(out, 'routington')
+          return glitter.copy('master', folder).then(function () {
+            assert(fs.existsSync(path.join(folder, 'package.json')))
+          })
+        })
+      })
+    })
+  })
+})
